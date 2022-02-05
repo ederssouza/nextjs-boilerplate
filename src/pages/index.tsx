b33c7e8e97c910b9e2151f5fe42c0261968ae5f4
Eder Sampaio <edersampaio@outlook.com.br>
2022-02-05 19:25:08 -0300

    Add husky config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,6 @@ type Props = {
   title?: string
 }
 
-const foo = 'bla'
-const foo2 = 'bla'
-
 export default function Home({ title = 'React Avançado' }: Props) {
   return (
     <div className={styles.container}>

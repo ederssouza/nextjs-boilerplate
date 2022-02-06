96e3198b95c042c7d7b926aaac3c20eaa6212b27
Eder Sampaio <edersampaio@outlook.com.br>
2022-02-06 10:03:29 -0300

    Add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+
+  body {
+    /* carrega a fonte padrão por sistema operacional */
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`
+
+export default GlobalStyle

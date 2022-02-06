2ded8db87d8bf7f719e65917ef11f8fd46ceca99
Eder Sampaio <edersampaio@outlook.com.br>
2022-02-06 17:47:33 -0300

    Add coverage test scripts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/(*.)?stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],

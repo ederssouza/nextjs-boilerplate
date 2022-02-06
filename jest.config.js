b2b01445cb758aa420db7a4951039b1869401fe8
Eder Sampaio <edersampaio@outlook.com.br>
2022-02-06 15:51:51 -0300

    Fix regex

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/(*.)?stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {

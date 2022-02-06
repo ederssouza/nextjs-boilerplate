5bbebdba048ca38a60b1ecbbc64c287d1f08164c
Eder Sampaio <edersampaio@outlook.com.br>
2022-02-06 12:12:45 -0300

    Remove dead code

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,6 @@ module.exports = {
     '../src/components/**/(*.)?stories.mdx',
     '../src/components/**/(*.)?stories.@(js|jsx|ts|tsx)'
   ],
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs'
-  ],
+  addons: ['@storybook/addon-essentials'],
   framework: '@storybook/react'
 }

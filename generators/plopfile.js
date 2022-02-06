588b5ea2be6b31457296257bc37455fe26f98d9e
Eder Sampaio <edersampaio@outlook.com.br>
2022-02-06 17:07:35 -0300

    Add Plop config

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -13,6 +13,21 @@ module.exports = function (plop) {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/index.tsx',
         templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs'
       }
     ]
   })

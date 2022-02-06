5fe4c138a5306607cf0dca57af481ae9e2e25974
Eder Sampaio <edersampaio@outlook.com.br>
2022-02-06 17:55:42 -0300

    Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/(*.)?stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/(*.)?stories.tsx',
+    '!src/components/*/index.ts',
+    '!src/pages/_app.tsx',
+    '!src/pages/_document.tsx',
+    '!src/types/**/*'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {

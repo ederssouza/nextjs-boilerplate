6a10bf3b2c73b7e5192a310a80fcbc56d26325f1
Eder Sampaio <edersampaio@outlook.com.br>
2022-08-18 18:02:02 -0300

    Fix config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 const settings = {
-  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
   pwa: {
     dest: 'public',
     disable: !isProd

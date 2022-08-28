31aafc97ba86e15097ae490608e1a9f94dbc9a24
Eder Sampaio <edersampaio@outlook.com.br>
2022-08-28 19:34:00 -0300

    Fix config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
-const baseConfig = {
-  reactStrictMode: true,
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
+/** @type {import('next').NextConfig} */
+const settings = {
   swcMinify: true,
   compiler: {
     styledComponents: true
   }
 }
 
-/** @type {import('next').NextConfig} */
-const nextConfig = isProd ? withPWA(baseConfig) : baseConfig
-
-module.exports = nextConfig
+module.exports = isProd ? withPWA(settings) : settings

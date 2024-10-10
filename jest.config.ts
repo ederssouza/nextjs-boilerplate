04386ff3e3a81fe5aef03eb11e1bd72844a36e85
Eder Sampaio <edersampaio@outlook.com.br>
2024-10-10 08:15:07 -0300

    Fix pipeline (#775)
    * fix: jest run
    
    * fix: node version
    
    * feat: update libs
    
    * chore: remove babel
    
    * fix: ignore styles dir
    
    * fix: ignore styles dir
    
    * feat: update libs
    
    * chore: remove babel

---
+++ jest.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        rootDir: '.'
+      }
+    }
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/(*.)?stories.tsx',
+    '!src/components/*/index.ts',
+    '!src/pages/_app.tsx',
+    '!src/pages/_document.tsx',
+    '!src/styles/**/*',
+    '!src/types/**/*'
+  ]
+}
+
+export default createJestConfig(config)
